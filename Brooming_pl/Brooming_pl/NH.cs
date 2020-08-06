156f93cae7cb4d846e5a777966f18a9b9d172815
LewMatt <lewmateush@gmail.com>
2020-08-06 14:24:45 +0200

    Files updated

---
+++ Brooming_pl/Brooming_pl/NH.cs
@@ -0,0 +1,64 @@
+﻿using Brooming_pl.Mapping;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+using NHibernate.Mapping.ByCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Brooming_pl
+{
+    public class NH
+    {
+        private static ISessionFactory SessionFactory;
+
+        private static List<Type> typeMappings = new List<Type>()
+        {
+
+            typeof(UsersMap),
+            typeof(RatingsMap),
+            typeof(CompanyMap),
+        };
+
+        public static void Init(string connectionString, List<Type> customTypes = null)
+        {
+            if (SessionFactory != null)
+                return;
+
+            Configuration config = new Configuration().DataBaseIntegration(db =>
+            {
+                db.Timeout = byte.MaxValue;
+                db.Dialect<MsSql2008Dialect>();
+                db.Driver<SqlClientDriver>();
+                db.ConnectionString = connectionString;
+                //db.Batcher<BatcherFactory>();
+            });
+
+            ModelMapper mapper = new ModelMapper();
+            mapper.AddMappings(typeMappings);
+
+
+            try
+            {
+
+                config.AddDeserializedMapping(mapper.CompileMappingForAllExplicitlyAddedEntities(), null);
+
+
+                SessionFactory = config.BuildSessionFactory();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public static ISession OpenSession()
+        {
+            return SessionFactory.OpenSession();
+        }
+
+    }
+}
