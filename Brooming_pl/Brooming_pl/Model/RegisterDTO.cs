97ad7973c91a210b67ac2945cb83c5e2f1611c7a
Cezary Śnieżek <33725870+Cezaryx@users.noreply.github.com>
2020-08-11 08:13:04 +0200

    changes

---
+++ Brooming_pl/Brooming_pl/Model/RegisterDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Brooming_pl.Model
+{
+    public class RegisterDTO
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string Adress { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Phonenumber { get; set; }
+        public string Email { get; set; }
+        public string LinkToAvatar { get; set; }
+    }
+}
--- Brooming_pl/Brooming_pl/Startup.cs
+++ Brooming_pl/Brooming_pl/Startup.cs
@@ -44,7 +44,7 @@ namespace Brooming_pl
 
             services.AddSwaggerGen();
         }
-
+ 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
